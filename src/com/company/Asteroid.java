2b93becbbc934e4c689efa53a52ee27f1cd526f8
Aleksejs Marmiss <aleksejs.marmiss@gmail.com>
2020-05-27 21:25:48 +0200

    [Universe] Asteroid behavior implemented

---
+++ src/com/company/Asteroid.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+
+public class Asteroid extends A_InteractableObject{
+
+    private int direction;
+    private int speed;
+
+
+
+    public Asteroid(int direction, int speed, int posX, int posY, int size, BufferedImage img ){
+        super(posX, posY, size, img);
+        this.direction = direction;
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+
+    @Override
+    public void update() {
+        if (this.direction == 1 || this.direction == 2){
+            this.posX += this.speed;
+            this.posY += this.speed;
+        }else {
+            this.posX -= this.speed;
+            this.posY += this.speed;
+        }
+
+    }
+
+    @Override
+    public void draw(Graphics gc) {
+        gc.drawImage(this.img, this.posX, this.posY,null);
+    }
+
+    public Rectangle getBounds(){
+        return new Rectangle(posX, posY, img.getWidth(),img.getHeight());
+    }
+
+}
+
+
